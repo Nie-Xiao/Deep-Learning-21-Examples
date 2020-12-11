b6fd0c4576741823f6c1715dbd60ebfaf93751b5
Nie-Xiao <64829656+Nie-Xiao@users.noreply.github.com>
2020-12-11 11:53:43 +0800

    Update save_pic.py

--- chapter_1/save_pic.py
+++ chapter_1/save_pic.py
@@ -1,9 +1,12 @@
 #coding: utf-8
+# 将MNIST数据集读取出来，并保存为图片文件，更直观的理解MNIST数据
 from tensorflow.examples.tutorials.mnist import input_data
+# scipy的misc作用是读取以数据转化并保存为图像
 import scipy.misc
 import os
 
-# 读取MNIST数据集。如果不存在会事先下载。
+# 读取MNIST数据集。如果不存在会事先下载到"MNIST_data/"文件夹中。可以到"MNIST_data/"文件夹下查看下载后的文件
+# 原始表示：1  独热表示：0，1，0，0，0，0，0，0，0，0
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # 我们把原始图片保存在MNIST_data/raw/文件夹下
@@ -19,6 +22,7 @@ for i in range(20):
     # TensorFlow中的MNIST图片是一个784维的向量，我们重新把它还原为28x28维的图像。
     image_array = image_array.reshape(28, 28)
     # 保存文件的格式为 mnist_train_0.jpg, mnist_train_1.jpg, ... ,mnist_train_19.jpg
+    # 这种图片序号的保存方法很不错
     filename = save_dir + 'mnist_train_%d.jpg' % i
     # 将image_array保存为图片
     # 先用scipy.misc.toimage转换为图像，再调用save直接保存。
